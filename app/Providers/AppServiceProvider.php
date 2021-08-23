3c9b46e9f6ee6fec107195dc55f31fbe305b63fe
Ata Kemal Atasoy <atakemalatasoy@hotmail.com.tr>
2021-08-23 07:42:00 +0300

    [ADD] DI registrations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use App\Core\Appointment\AppointmentManager;
+use App\Core\Auth\TokenHandler;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +26,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->singleton(TokenHandler::class, function($app){
+            return new TokenHandler(
+                env('JWT_SECRET'), 
+                env('JWT_EXPIRY_IN_SECONDS'), 
+                env('APP_NAME')
+            );
+        });
+
+        $this->app->singleton(AppointmentManager::class, function($app){
+            $user = request()->get('user');
+            //Incase shit goes sideways, it never does tho
+            if(is_null($user)) throw new \Exception("This is a serious exception!");
+
+            return new AppointmentManager($user, env('POSTAL_CODE'));
+        });
     }
 }
