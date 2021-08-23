b4432e3a5d9ca7c8d7363c8433dfd1d88f313777
Ata Kemal Atasoy <atakemalatasoy@hotmail.com.tr>
2021-08-23 07:41:53 +0300

    [ADD] model additions and migration changes

--- database/migrations/2021_08_22_200026_create_contacts_table.php
+++ database/migrations/2021_08_22_200026_create_contacts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateContactsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('contacts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email', 255);
+            $table->string('phone', 255);
             $table->timestamps();
+
+            $table->unique(['first_name', 'last_name', 'email']);
         });
     }
 
@@ -29,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('contacts');
     }
 }
