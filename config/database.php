482683f3a27cbf4a701206315075f90e2c4e31d7
Ata Kemal Atasoy <atakemalatasoy@hotmail.com.tr>
2021-09-03 18:17:18 +0300

    [UPDATE] db config for heroku

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$credentials = parse_url(env("CLEARDB_DATABASE_URL"));
+
 return [
 
     /*
@@ -46,11 +48,15 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            // 'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            // 'database' => env('DB_DATABASE', 'forge'),
+            // 'username' => env('DB_USERNAME', 'forge'),
+            // 'password' => env('DB_PASSWORD', ''),
+            'host' => $credentials['host'],
+            'username' => $credentials['user'],
+            'password' => $credentials['pass'],
+            'database' => substr($credentials["path"], 1),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
